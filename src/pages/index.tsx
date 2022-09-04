7a0db6bcc981d458f4467ffb48572288ea0c9404
Altay Batuhan <altaybatuhanmail@gmail.com>
2022-09-04 09:00:03 -0700

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Arda Guler Kac Dakika Oynadi</title>
+        <meta name="description" content="Arda Guler Kac Dakika Oynadi" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div className="z-0 absolute w-screen h-screen bg-[url('/ardaguler.jpg')] bg-cover bg-top brightness-50 contrast-50 hue-rotate-30 saturate-150" />
+      <div className="z-10 absolute w-screen h-screen flex flex-col justify-center items-center bg-white/30 font-['Nunito']">
+        <h2 className="text-xl text-[#163962] font-bold">Arda Guler Kac Dakika Oynadi?</h2>
+        <h1 className="text-xl sm:text-9xl text-[#ffed00] my-5">146 Dakika</h1>
+        <h2 className="text-xl text-[#163962] font-bold">1500 Dakika Yolunda %9.73</h2>
+      </div>
+      <footer className="z-10 absolute w-screen text-center bottom-0 inset-x-0 font-['Nunito']">
+        <h1 className="text-sm text-stone-400 bg-[#163962]/75">Fan sitesidir, telif sikintisi olursa lutfen altaybatuhanmail@gmail.com uzerinden ulasin, SANLI FENERBAHCE</h1>
+      </footer>
+    </>
+  );
+};
+
+export default Home;
